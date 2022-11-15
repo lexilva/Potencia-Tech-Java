13998d2f197a96cfec6f9ce01e6049865e898ecf
Alex Sandro <lexilva@gmail.com>
2022-11-15 16:51:48 -0300

    Mentoria e Curso

--- Java Básico/desafioPoo/dasafio-dio-poo/src/Main.java
+++ Java Básico/desafioPoo/dasafio-dio-poo/src/Main.java
@@ -1,5 +1,26 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "Hello world!" );
+        Curso curso1 = new Curso();
+        curso1.setTitulo( "curso java" );
+        curso1.setDescricao( "descrição curso java" );
+        curso1.setCargaHoraria( 8 );
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo( "curso js" );
+        curso2.setDescricao( "descição curso js" );
+        curso2.setCargaHoraria( 4 );
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTiatulo( "mentoria de java" );;
+        mentoria.setDescricao( "descrição mentoria java" );
+        mentoria.setData( LocalDate.now() );
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
